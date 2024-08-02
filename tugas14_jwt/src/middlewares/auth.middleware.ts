d0591dad5a767c97e90f7cf7eeaa2e37874fb06d
yusufkamal-ok <59947329+yusufkamal-ok@users.noreply.github.com>
2024-08-02 21:59:43 +0700

    add tugas14

---
+++ tugas14_jwt/src/middlewares/auth.middleware.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from "express";
+const jwt = require('jsonwebtoken');
+import { SECRET } from "@/utils/env";
+import { IReqUser } from "@/utils/interface";
+
+export default (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  const [prefix, accessToken] = token.split(" ");
+
+  if (prefix !== "Bearer" || !accessToken) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  const user = jwt.verify(accessToken, SECRET) as { id: string; role: string };
+
+  if (!user) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  (req as IReqUser).user = {
+    id: user.id,
+    role: user.role,
+  };
+
+  next();
+};
